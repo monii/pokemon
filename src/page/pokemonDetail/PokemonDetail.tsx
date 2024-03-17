4e05d0f7f5fb8e77265685d00bca715475c91c07
monii <moniyel@gmail.com>
2024-03-18 03:42:39 +0900

    refact: 자잘한 ui수정 - Loading 컴포넌트 분리 - meta태그 추가 - read.ME 변경

--- src/page/pokemonDetail/PokemonDetail.tsx
+++ src/page/pokemonDetail/PokemonDetail.tsx
@@ -9,13 +9,32 @@ import Evolution from "./components/Evolution";
 import usePokemonStore from "../../store/pokemon";
 import { GetPokemonSpeciesDTO } from "../../types/pokemonSpecies";
 import { FaLongArrowAltLeft } from "react-icons/fa";
-import { convertNameToKoean, convertToNumber, convertTypeToKoean, findColorByType } from "../../util/utile";
+import {
+  convertNameToKoean,
+  convertToNumber,
+  convertTypeToKoean,
+  findColorByType,
+} from "../../util/utile";
 
-const PokemonDetailContainer = styled.div<{ bordercolor: string }>`
+const PokemonDetailContainer = styled.div`
+  position: relative;
+`;
+const PokemonDetailChildren = styled.div`
   padding: 12px 20px;
   max-width: 720px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, 50%);
   border-radius: 10px;
   box-shadow: 0 5px 15px 4px rgb(0 0 0/30%);
+
+  @media (max-width: 390px) {
+    position: static;
+    top: 0%;
+    left: 0%;
+    transform: translate(0%, 0%);
+  }
 `;
 const ContentSection = styled.section`
   display: flex;
@@ -44,8 +63,7 @@ const TypeWrapper = styled.div`
   padding-bottom: 12px;
 `;
 const BackButtonWrapper = styled.section``;
-const BackButton = styled.button`
-`;
+const BackButton = styled.button``;
 
 function PokemonDetail() {
   const navigate = useNavigate();
@@ -74,42 +92,42 @@ function PokemonDetail() {
   };
 
   return (
-    <PokemonDetailContainer
-      bordercolor={findColorByType(
-        pokemonDetailInfo?.types[0].type.name || "default"
-      )}
-    >
-      <BackButtonWrapper>
-        <BackButton>
-          <FaLongArrowAltLeft size="40" onClick={clickBackButton} />
-          <p>뒤로가기</p>
-        </BackButton>
-      </BackButtonWrapper>
-      <ContentSection>
-        <ImageSection>
-          <ImageWrapper>
-            <PokemonImage
-              loading="lazy"
-              alt={pokemonDetailInfo?.name}
-              src={pokemonDetailInfo?.sprites?.other.dream_world.front_default}
-            />
-          </ImageWrapper>
-        </ImageSection>
-        <InfoWrapper>
-          <NameText>{convertNameToKoean(pokemonSpeciesInfo?.names)}</NameText>
-          <TypeWrapper>
-            {pokemonDetailInfo?.types.map((type) => (
-              <Chip
-                key={type.type.name}
-                type={convertTypeToKoean(type.type.name)}
-                backgroundcolor={findColorByType(type.type.name)}
-                borderRadius={5}
+    <PokemonDetailContainer>
+      <PokemonDetailChildren>
+        <BackButtonWrapper>
+          <BackButton>
+            <FaLongArrowAltLeft size="40" onClick={clickBackButton} />
+            <p>뒤로가기</p>
+          </BackButton>
+        </BackButtonWrapper>
+        <ContentSection>
+          <ImageSection>
+            <ImageWrapper>
+              <PokemonImage
+                loading="lazy"
+                alt={pokemonDetailInfo?.name}
+                src={
+                  pokemonDetailInfo?.sprites?.other.dream_world.front_default
+                }
               />
-            ))}
-          </TypeWrapper>
-          <Evolution  />
-        </InfoWrapper>
-      </ContentSection>
+            </ImageWrapper>
+          </ImageSection>
+          <InfoWrapper>
+            <NameText>{convertNameToKoean(pokemonSpeciesInfo?.names)}</NameText>
+            <TypeWrapper>
+              {pokemonDetailInfo?.types.map((type) => (
+                <Chip
+                  key={type.type.name}
+                  type={convertTypeToKoean(type.type.name)}
+                  backgroundcolor={findColorByType(type.type.name)}
+                  borderRadius={5}
+                />
+              ))}
+            </TypeWrapper>
+            <Evolution />
+          </InfoWrapper>
+        </ContentSection>
+      </PokemonDetailChildren>
     </PokemonDetailContainer>
   );
 }
